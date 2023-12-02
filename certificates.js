4a689df47c882a3071fe23e70e84ba4729f0c729
sahilsalam25 <sahilsalamece25@gmail.com>
2023-12-02 10:46:03 +0530

    FRONT-END_CERTIFICATES

---
+++ certificates.js
@@ -0,0 +1,44 @@
+console.log(`Hello Everyone ,This is SAHIL SALAM :
+"I'm thrilled to announce the successful completion of my Front-End Development course! Demonstrating proficiency in various front-end development skills and technologies. 
+[Ultimate Front-End Bootcamp,Front-End Developement, React.JS,JavaScript, CSS: ]
+🚀 Excited to apply my new skills in creating seamless and visually stunning user experiences". 
+Ready to take on the world of web development! 💻🎉
+" #FrontEndDeveloper #CodingJourney #WebDevelopment #frontenddevelopment #frontend #frontendwebdevelopment #css3 #js #javascriptdeveloper #javascript #bootstrap #reactjs #reactjsdeveloper #udemy #udemycourse #udemycertificate #React.js #Simplilearn #Skillup "`);
+
+
+
+console.log('Below Shown are some Skills Related to Front-End Developement:');
+
+
+let mySkills=`
+1- HTML5 and CSS3: Developed strong foundations in creating structured and visually appealing web pages using the latest standards in HTML and CSS.
+
+2- JavaScript: Acquired in-depth knowledge of JavaScript programming, including DOM manipulation, event handling, and asynchronous programming.
+
+3- Responsive Web Design: Mastered the principles of responsive design, ensuring seamless user experiences across various devices and screen sizes.`;
+console.log(mySkills);
+
+
+
+console.log('Below Shown are some PROJECTS Related to Front-End Developement:');
+
+
+let myProjects=` PROJECTS -:
+1- React CALCULATOR APP | React.js, JavaScript, CSS, HTML5, VS Code          2023
+• Designed and implemented an intuitive user interface for the calculator, ensuring a seamless and user-friendly experience.
+• Implemented the core functionality for performing arithmetic operations, including addition, subtraction, multiplication,
+and division.
+2- MYNTRA APP CLONE | JavaScript, CSS, HTML5, VS Code                         2023
+• Implemented a user-friendly and responsive design, closely mirroring the Myntra’s Website layout and navigation.
+• Implemented a shopping cart system with features such as adding/removing items, updating quantities, and calculating
+order totals.
+3- TO DO LIST APP | JavaScript, HTML5, CSS,VS Code                            2023
+• Designed a functionality for adding, updating, and removing tasks dynamically using JavaScript.
+• Integrated local storage to persist user tasks, allowing users to revisit and manage their to-do items even after refreshing
+the page.
+4- REGISTRATION FORM | FreeCodeCamp, CSS, HTML5, VS Code                       2023
+• Implemented form validation to ensure accurate and complete user input, enhancing data quality ,to improve data
+accuracy and user interaction.
+5- CAFE MENU,CAT PHOTO APP,COLOR CODE | FreeCodeCamp,CSS,HTML5,VS Code           2023
+• Demonstrated proficiency in front-end web development, showcasing attention to design and user experience.`;
+console.log(myProjects);
